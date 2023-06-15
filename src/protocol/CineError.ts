eb89f1552fb993f69fb0b47aba9679ab49814ea6
Jose D <joserafael161@hotmail.com>
2023-06-15 00:05:03 -0500

    Use status codes from a different repo

--- src/protocol/CineError.ts
+++ src/protocol/CineError.ts
@@ -1,45 +1,37 @@
-import { constants as HttpConstants } from "http2";
+import { StatusCodes } from "http-status-codes";
 
 let errorCodeCount = 4003;
 export default class CineError {
-  public static readonly NOT_AUTHORIZED = new CineError(
-    "Not authorized",
-    HttpConstants.HTTP_STATUS_FORBIDDEN,
-    errorCodeCount++,
-  );
+  public static readonly NOT_AUTHORIZED = new CineError("Not authorized", StatusCodes.FORBIDDEN, errorCodeCount++);
   public static readonly INVALID_BODY_FORMAT = new CineError(
     "Invalid body format",
-    HttpConstants.HTTP_STATUS_BAD_REQUEST,
+    StatusCodes.BAD_REQUEST,
     errorCodeCount++,
   );
   public static readonly INVALID_TORRENT = new CineError(
     "Error creating torrent",
-    HttpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR,
+    StatusCodes.INTERNAL_SERVER_ERROR,
     errorCodeCount++,
   );
   public static readonly NOT_FOUND_OR_NOT_ACTIVE_TORRENT = new CineError(
     "Torrent not found or not active",
-    HttpConstants.HTTP_STATUS_NOT_FOUND,
+    StatusCodes.NOT_FOUND,
     errorCodeCount++,
   );
-  public static readonly PAGE_NOT_FOUND = new CineError(
-    "Not found",
-    HttpConstants.HTTP_STATUS_NOT_FOUND,
-    errorCodeCount++,
-  );
+  public static readonly PAGE_NOT_FOUND = new CineError("Not found", StatusCodes.NOT_FOUND, errorCodeCount++);
   public static readonly INVALID_FILE_INDEX = new CineError(
     "Invalid file index",
-    HttpConstants.HTTP_STATUS_NOT_FOUND,
+    StatusCodes.NOT_FOUND,
     errorCodeCount++,
   );
   public static readonly INVALID_CREDENTIALS = new CineError(
     "Invalid credentials",
-    HttpConstants.HTTP_STATUS_UNAUTHORIZED,
+    StatusCodes.UNAUTHORIZED,
     errorCodeCount++,
   );
   public static readonly INTERNAL_ERROR = new CineError(
     "Something went wrong",
-    HttpConstants.HTTP_STATUS_INTERNAL_SERVER_ERROR,
+    StatusCodes.INTERNAL_SERVER_ERROR,
     errorCodeCount++,
   );
   constructor(public errorMessage: string, public httpStatusCode: number, public errorCode: number) {}
